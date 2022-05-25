de9571dc9a32eca83c0e677b0e4e04be0b4030a5
Gino Ochoa <gochoa1088@gmail.com>
2022-05-25 11:34:59 -0700

    initial and completed commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c4f2e042-46e3-4dd0-bdf5-7547acf89b2b';
+export const MODAL_CLOSE_SEC = 2.5;
