20dbffc54bc2c622abe6b745970cedcda5ee2f12
Gino <40076376+gochoa1088@users.noreply.github.com>
2022-08-04 10:08:11 -0700

    View.js cleanup

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -30,18 +30,14 @@ export default class View {
 
     newElements.forEach((newEl, i) => {
       const curEl = curElements[i];
-      //   console.log(curEl, newEl.isEqualNode(curEl));
 
-      // Updates text
       if (
         !newEl.isEqualNode(curEl) &&
         newEl.firstChild?.nodeValue.trim() !== ''
       ) {
-        // console.log('😎', newEl.firstChild.nodeValue.trim());
         curEl.textContent = newEl.textContent;
       }
 
-      // Update attributes
       if (!newEl.isEqualNode(curEl)) {
         Array.from(newEl.attributes).forEach(attr =>
           curEl.setAttribute(attr.name, attr.value)
